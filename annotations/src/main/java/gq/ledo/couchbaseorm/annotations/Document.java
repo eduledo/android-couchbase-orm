fda80d5de01adb5f3b87d5304df23479344e6e5c
Eduardo Ledo <edu@ledo.gq>
2017-11-14 06:07:50 -0300

    Annotation

---
+++ annotations/src/main/java/gq/ledo/couchbaseorm/annotations/Document.java
@@ -0,0 +1,14 @@
+package gq.ledo.couchbaseorm.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Eduardo Ledo <eduardo.ledo@gmail.com> on 14/11/2017.
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
+public @interface Document {
+}
---
+++ annotations/src/main/java/gq/ledo/couchbaseorm/annotations/Property.java
@@ -0,0 +1,15 @@
+package gq.ledo.couchbaseorm.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Eduardo Ledo <eduardo.ledo@gmail.com> on 14/11/2017.
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.FIELD)
+public @interface Property {
+    String value();
+}
